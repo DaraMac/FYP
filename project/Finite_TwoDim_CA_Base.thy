a482492504c3bd5ee0f5bf5677af843bba449a65
dmac <DaraMac@users.noreply.github.com>
2020-04-09 15:37:33 +0100

    Add beginnings of nbhd function for finite 2D, add utility nb and list funtions to 2D base

--- project/Finite_TwoDim_CA_Base.thy
+++ project/Finite_TwoDim_CA_Base.thy
@@ -6,21 +6,56 @@ type_synonym state = "cell list list"
 
 datatype CA = CA (State : state) (Rule : rule)
 
+definition width :: "state \<Rightarrow> nat" where
+"width s \<equiv> length s"
+
+definition height :: "state \<Rightarrow> nat" where
+"height s = length (hd s)"
+
+
+value "[(x, y). x \<leftarrow> [0..<10], y \<leftarrow> [0..<10]]"
+
 fun inner_nbhds :: "state \<Rightarrow> neighbourhood list" where
 "inner_nbhds (x#y#z#[]) = (Nb x y z) # []" |
 "inner_nbhds (x#y#z#zs) = (Nb x y z) # (inner_nbhds (y#z#zs))" |
 "inner_nbhds _ = []" (* neighbourhoods aren't defined for \<le> 2 *)
 
+fun get_cell :: "state \<Rightarrow> (nat\<times>nat) \<Rightarrow> cell" where
+"get_cell s (x,y) = s!x!y"
+
+
+value "[(nat (1+i) mod 3, nat (1+j) mod 3). j\<leftarrow>rev [-1..1], i\<leftarrow>[-1..1]]"
+
+fun get_nbhd :: "state \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> neighbourhood" where
+"get_nbhd s x y = (let w = width s in list_to_nb )"
+
+fun nbhds :: "state \<Rightarrow> neighbourhood list list" where
+"nbhds s = [[get_nbhd s x y. y \<leftarrow> [0..<height ca]]. x \<leftarrow> [0..<width s]]"
+
+fun nbhd :: "state \<Rightarrow> neighbourhood list" where
+"nbhd [] = []" | (*this should never happen*)
+"nbhd (x#xs) = ((Nb (last xs) x (hd xs)) # (inner_nbhds (x#xs))) @ [Nb (last (butlast xs)) (last xs) x]"
+
+
+fun update_CA :: "CA \<Rightarrow> CA" where
+"update_CA (CA s r) = CA (map r (nbhds s)) r"
+
+fun run_t_steps :: "CA \<Rightarrow> nat \<Rightarrow> CA" where
+"run_t_steps ca n = apply_t_times update_CA ca n"
+
 
 subsection \<open>Simple properties for CA\<close>
 
-definition width :: "CA \<Rightarrow> nat" where
-"width ca = length (State ca)"
+definition widthCA :: "CA \<Rightarrow> nat" where
+"widthCA ca = width (State ca)"
+
+definition heightCA :: "CA \<Rightarrow> nat" where
+"heightCA ca = height (State ca)"
 
 definition wellformed :: "CA \<Rightarrow> bool" where
-"wellformed ca \<equiv> (width ca \<ge> 3)"
+"wellformed ca \<equiv> (width ca \<ge> 3) \<and> (height ca \<ge> 3) \<and> (\<forall> i. length ((State ca) ! (i mod (width ca))) = height ca)"
 
-(* doesnt hold for empty list *)
+(* doesnt work for 2D currently *)
 definition uniform :: "state \<Rightarrow> bool" where
 "uniform s \<equiv> length (remdups s) = 1"
 end
--- project/TwoDim_CA_Base.thy
+++ project/TwoDim_CA_Base.thy
@@ -11,14 +11,19 @@ type_synonym rule = "neighbourhood \<Rightarrow> cell"
 fun apply_nb :: "(cell \<Rightarrow> cell) \<Rightarrow> neighbourhood \<Rightarrow> neighbourhood" where
 "apply_nb f (Nb nw n ne w c e sw s se) = Nb (f nw) (f n) (f ne) (f w) (f c) (f e) (f sw) (f s) (f se)"
 
-fun list_nb :: "neighbourhood \<Rightarrow> cell list" where
-"list_nb (Nb nw n ne w c e sw s se) = [nw, n, ne, w, c, e, sw, s, se]"
+fun nb_to_list :: "neighbourhood \<Rightarrow> cell list" where
+"nb_to_list (Nb nw n ne w c e sw s se) = [nw, n, ne, w, c, e, sw, s, se]"
+
+(* intentionally not total *)
+fun list_to_nb :: "cell list \<Rightarrow> neighbourhood" where
+"list_to_nb [nw, n, ne, w, c, e, sw, s, se] = Nb nw n ne w c e sw s se"
+
 
 fun flip_nb :: "neighbourhood \<Rightarrow> neighbourhood" where
 "flip_nb nb = apply_nb flip nb"
 
 fun sum_nb :: "neighbourhood \<Rightarrow> nat" where
-"sum_nb nb = count_list (list_nb nb) One"
+"sum_nb nb = count_list (nb_to_list nb) One"
 
 fun complement :: "rule \<Rightarrow> rule" where
 "complement r nb = flip (r (flip_nb nb))"
