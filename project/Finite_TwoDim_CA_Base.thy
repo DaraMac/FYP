16515deabb437bdf306895c38cf1a19cda2dad6a
dmac <DaraMac@users.noreply.github.com>
2020-04-09 18:40:56 +0100

    Make horrible definition for finite 2D work

--- project/Finite_TwoDim_CA_Base.thy
+++ project/Finite_TwoDim_CA_Base.thy
@@ -13,35 +13,36 @@ definition height :: "state \<Rightarrow> nat" where
 "height s = length (hd s)"
 
 
-value "[(x, y). x \<leftarrow> [0..<10], y \<leftarrow> [0..<10]]"
+definition oneCentre :: state where
+"oneCentre \<equiv> [[Zero, Zero, Zero], [Zero, One, Zero], [Zero, Zero, Zero]]"
 
-fun inner_nbhds :: "state \<Rightarrow> neighbourhood list" where
-"inner_nbhds (x#y#z#[]) = (Nb x y z) # []" |
-"inner_nbhds (x#y#z#zs) = (Nb x y z) # (inner_nbhds (y#z#zs))" |
-"inner_nbhds _ = []" (* neighbourhoods aren't defined for \<le> 2 *)
-
-fun get_cell :: "state \<Rightarrow> (nat\<times>nat) \<Rightarrow> cell" where
-"get_cell s (x,y) = s!x!y"
-
+fun get_cell :: "state \<Rightarrow> int \<Rightarrow> int \<Rightarrow> cell" where
+"get_cell s x y = s!(nat x)!(nat y)"
 
 value "[(nat (1+i) mod 3, nat (1+j) mod 3). j\<leftarrow>rev [-1..1], i\<leftarrow>[-1..1]]"
 
-fun get_nbhd :: "state \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> neighbourhood" where
-"get_nbhd s x y = (let w = width s in list_to_nb )"
+fun get_nbhd :: "state \<Rightarrow> int \<Rightarrow> int \<Rightarrow> neighbourhood" where
+"get_nbhd s x y = (let w = int (width s) in (let h = int (height s) in
+ list_to_nb [get_cell s ((x+i) mod w) ((y+j) mod h). j \<leftarrow> rev [-1..1], i \<leftarrow> [-1..1]]))"
 
 fun nbhds :: "state \<Rightarrow> neighbourhood list list" where
-"nbhds s = [[get_nbhd s x y. y \<leftarrow> [0..<height ca]]. x \<leftarrow> [0..<width s]]"
+"nbhds s = (let h = int ((height s)-1) in (let w = int ((width s)-1) in
+ [[get_nbhd s x y. y \<leftarrow> [0..h]]. x \<leftarrow> [0..w]]))"
 
-fun nbhd :: "state \<Rightarrow> neighbourhood list" where
-"nbhd [] = []" | (*this should never happen*)
-"nbhd (x#xs) = ((Nb (last xs) x (hd xs)) # (inner_nbhds (x#xs))) @ [Nb (last (butlast xs)) (last xs) x]"
-
-
+ 
 fun update_CA :: "CA \<Rightarrow> CA" where
-"update_CA (CA s r) = CA (map r (nbhds s)) r"
+"update_CA (CA s r) = CA (map (\<lambda> xs. map r xs) (nbhds s)) r"
 
 fun run_t_steps :: "CA \<Rightarrow> nat \<Rightarrow> CA" where
 "run_t_steps ca n = apply_t_times update_CA ca n"
+
+
+definition blinker :: state where
+"blinker \<equiv> [replicate 5 Zero,
+ [Zero ,Zero, One, Zero, Zero],
+ [Zero, Zero, One, Zero, Zero],
+ [Zero, Zero, One, Zero, Zero],
+ replicate 5 Zero]"
 
 
 subsection \<open>Simple properties for CA\<close>
@@ -52,8 +53,8 @@ definition widthCA :: "CA \<Rightarrow> nat" where
 definition heightCA :: "CA \<Rightarrow> nat" where
 "heightCA ca = height (State ca)"
 
-definition wellformed :: "CA \<Rightarrow> bool" where
-"wellformed ca \<equiv> (width ca \<ge> 3) \<and> (height ca \<ge> 3) \<and> (\<forall> i. length ((State ca) ! (i mod (width ca))) = height ca)"
+definition wellformed :: "state \<Rightarrow> bool" where
+"wellformed s \<equiv> (width s \<ge> 3) \<and> (height s \<ge> 3) \<and> (\<forall> i. length (s ! (i mod (width s))) = height s)"
 
 (* doesnt work for 2D currently *)
 definition uniform :: "state \<Rightarrow> bool" where
