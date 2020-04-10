87c89b4db51d5ccc019d7e29e83aa1d40980f3d8
dmac <DaraMac@users.noreply.github.com>
2020-04-10 17:46:23 +0100

    Remove all Properties files

--- project/Bounded_TwoDim_CA.thy
+++ project/Bounded_TwoDim_CA.thy
@@ -23,4 +23,38 @@ fun update_CA :: "CA \<Rightarrow> CA" where
 
 fun run_t_steps :: "CA \<Rightarrow> nat \<Rightarrow> CA" where
 "run_t_steps ca n = apply_t_times update_CA ca n"
+
+
+subsection \<open>Properties\<close>
+
+definition stable :: "CA \<Rightarrow> bool" where
+"stable ca \<equiv> State (update_CA ca) = State ca"
+
+definition uniform :: "state \<Rightarrow> bool" where
+"uniform s \<equiv> length (remdups (concat s)) = 1"
+
+definition yields :: "CA \<Rightarrow> state \<Rightarrow> bool" (infixr \<open>yields\<close>  65) where
+"A yields s \<equiv> (\<exists> n. State (run_t_steps A n) = s \<and> n > 0)"
+
+definition loops :: "CA \<Rightarrow> bool" where
+"loops ca \<equiv> ca yields State ca"
+
+fun reversible :: "CA \<Rightarrow> bool" where
+"reversible (CA _ r) = (\<forall>s. (\<exists>!s0. State (update_CA (CA s0 r)) = s))"
+
+fun garden_of_eden :: "CA \<Rightarrow> bool" where
+"garden_of_eden (CA s r) = (\<not>(\<exists> s0. State (update_CA (CA s0 r)) = s))"
+
+lemma "garden_of_eden ca \<Longrightarrow> \<not>reversible ca"
+  by (metis garden_of_eden.elims(2) reversible.simps)
+
+
+definition class1 :: "rule \<Rightarrow> bool" where
+"class1 r \<equiv> (\<exists>! f. (\<forall> s. (CA s r) yields f \<and> uniform f \<and> stable (CA f r)))"
+
+definition class2 :: "rule \<Rightarrow> bool" where
+"class2 r \<equiv> (\<forall> s. (\<exists> f. (CA s r) yields f \<and> loops (CA f r)))"
+
+lemma "class1 ca \<Longrightarrow> class2 ca"
+  using class1_def class2_def loops_def by auto
 end
--- project/Toroidal_Elementary_CA.thy
+++ project/Toroidal_Elementary_CA.thy
@@ -22,4 +22,55 @@ subsection \<open> Basic CA Rule Examples \<close>
 
 definition rule110 :: CA where
 "rule110 \<equiv> CA [Zero, One, Zero] r110"
+
+
+subsection \<open>Properties\<close>
+
+definition stable :: "CA \<Rightarrow> bool" where
+"stable ca \<equiv> State (update_CA ca) = State ca"
+
+definition yields :: "CA \<Rightarrow> state \<Rightarrow> bool" (infixr \<open>yields\<close>  65) where
+"A yields s \<equiv> (\<exists> n. State (run_t_steps A n) = s \<and> n > 0)"
+
+definition loops :: "CA \<Rightarrow> bool" where
+"loops ca \<equiv> ca yields State ca"
+
+(* TODO reframe in terms of rules only *)
+fun reversible :: "CA \<Rightarrow> bool" where
+"reversible (CA _ r) = (\<forall>s. (\<exists>!s0. State (update_CA (CA s0 r)) = s))"
+
+theorem "ca yields State (run_t_steps ca 1)"
+proof-
+  show ?thesis using yields_def by blast
+qed
+
+theorem t1 :"n>0 \<Longrightarrow> ca yields State (run_t_steps ca n)"
+  apply(simp add: yields_def)
+  apply(rule exI)
+  apply(rule conjI)
+  apply(auto)
+  done
+
+
+(* maybe add some infix notation for this or other things as it looks good *)
+fun garden_of_eden :: "CA \<Rightarrow> bool" where
+"garden_of_eden (CA s r) = (\<not>(\<exists> s0. State (update_CA (CA s0 r)) = s))"
+
+definition orphan :: "state \<Rightarrow> rule \<Rightarrow> bool" where
+"orphan s0 r = (\<forall> sl sr. garden_of_eden (CA (sl@s0@sr) r))"
+
+lemma "garden_of_eden ca \<Longrightarrow> \<not>reversible ca"
+  apply (metis garden_of_eden.elims(2) reversible.simps)
+done
+
+(*lemma "garden_of_eden (CA s r) \<Longrightarrow> (\<exists> s0. (orphan s0 r) \<and> (\<exists> sl sr. (sl@s0@sr) = s))"*)
+
+definition class1 :: "rule \<Rightarrow> bool" where
+"class1 r \<equiv> (\<exists>! f. (\<forall> s. (CA s r) yields f \<and> uniform f \<and> stable (CA f r)))"
+
+definition class2 :: "rule \<Rightarrow> bool" where
+"class2 r \<equiv> (\<forall> s. (\<exists> f. (CA s r) yields f \<and> loops (CA f r)))"
+
+lemma "class1 ca \<Longrightarrow> class2 ca"
+  using class1_def class2_def loops_def by auto
 end
